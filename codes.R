5ee70704b4d6e3be7f880dcb07620ecd2d078d0c
Angeline Ng <Yi.Ng@warwick.ac.uk>
2023-12-06 02:36:55 +0000

    More edits. Rmd to be rewriten completely based on new data processing approach

--- codes.R
+++ codes.R
@@ -8,6 +8,7 @@ library(stringr)
 library(purrr)
 library(tidytext)
 library(textdata)
+library(glmnet)
 
 #Clear
 cat("\014")  
@@ -27,9 +28,41 @@ user_data  <- load(file='yelp_user_small.Rda')
 
 #Cleaning & Rectangling data
 
+#create sentiment scores from review text
+## Get the sentiment lexicon
+sentiments <- lexicon_afinn()
+## Tokenise the text
+tokens <- review_data_small %>%
+  unnest_tokens(word, text)
+## Calculate sentiment scores
+sentiment_scores <- tokens %>%
+  inner_join(sentiments) %>%
+  group_by(review_id) %>%
+  summarise(sentiment_score = sum(value))
+#Add sentiment scores to review_data_small
+review_data_small <- left_join(review_data_small, sentiment_scores, by = "review_id")
+
+##Transform date variables to dttm format
+review_data_small$date <- parse_datetime(review_data_small$date, format = "%Y-%m-%d %H:%M:%S", na = c("", "NA"), locale = default_locale(), trim_ws = TRUE)
+user_data_small$yelping_since <- parse_datetime(user_data_small$yelping_since, format = "%Y-%m-%d %H:%M:%S", na = c("", "NA"), locale = default_locale(), trim_ws = TRUE)
+tip_data$date <- parse_datetime(tip_data$date, format = "%Y-%m-%d %H:%M:%S", na = c("", "NA"), locale = default_locale(), trim_ws = TRUE)
+
+# Drop the text column
+review_data_small <- review_data_small %>%
+  select(-text)
+
 ## Flatten attributes and hours in business_data
 attributes <- flatten(business_data$attributes)
 business_data <- cbind(business_data, attributes)
+
+## Covert string variables to boolean
+business_data <- business_data %>% mutate_at(vars(ByAppointmentOnly, BusinessAcceptsCreditCards, RestaurantsTakeOut, RestaurantsDelivery, Caters, RestaurantsReservations, OutdoorSeating, GoodForKids, DogsAllowed, RestaurantsGoodForGroups), as.logical)
+
+business_data <- business_data %>%
+  mutate(FreeWiFi = ifelse(WiFi == "u'free'", "true", 
+                           ifelse(WiFi == "u'no'", "false", NA)))
+
+business_data$FreeWiFi <- as.logical(business_data$FreeWiFi) # Convert the 'FreeWiFi' column to logical
 
 ## Globalise business category variable into factors
 # Define the list of business categories (source: https://blog.yelp.com/businesses/yelp_category_list/)
@@ -37,50 +70,36 @@ business_category <- c("Active Life", "Arts & Entertainment", "Automotive", "Bea
 
 # Create a new variable that maps the business category
 business_data <- business_data %>%
-  mutate(category = map(categories, ~str_extract(., paste(business_category, collapse = "|"))))
-
+  mutate(category = map_chr(categories, ~str_extract(., paste(business_category, collapse = "|"))))
+  
 # Keep only relevant variables in business_data
 business_data <- business_data %>%
-  select(business_id, city, state, postal_code, latitude, longitude, stars, review_count, is_open, ByAppointmentOnly, BusinessAcceptsCreditCards, RestaurantsPriceRange2, RestaurantsTakeOut, RestaurantsDelivery, Caters, WiFi, Ambience, RestaurantsReservations, OutdoorSeating, GoodForMeal, GoodForKids, DogsAllowed, RestaurantsGoodForGroups, NoiseLevel)
+  select(business_id, name, category, city, state, postal_code, latitude, longitude, stars, review_count, ByAppointmentOnly, BusinessAcceptsCreditCards, RestaurantsPriceRange2, RestaurantsTakeOut, RestaurantsDelivery, Caters, FreeWiFi, BusinessParking, WheelchairAccessible, Ambience, RestaurantsReservations, OutdoorSeating, GoodForMeal, GoodForKids, DogsAllowed, Alcohol, RestaurantsAttire, RestaurantsGoodForGroups, NoiseLevel)
 
 ##Generate frequency of business check-ins to indicate users engagement. 
 ##Check-ins are usually motivated by offers initiated by the businesses. 
 ##Business with more frequent check-ins are more likely to receive high reviews as these reviews were incentivised by promotional deals.
-checkin_data$date <- strsplit(checkin_data$date, split = ",")
 checkin_data$checkin_freq <- sapply(checkin_data$date, length)
 
 #Drop checkin_data$date
 checkin_data <- checkin_data %>%
   select(-date)
 
-##Transform date variables to dttm format
-review_data_small$date <- parse_datetime(review_data_small$date, format = "%Y-%m-%d %H:%M:%S", na = c("", "NA"), locale = default_locale(), trim_ws = TRUE)
-user_data_small$yelping_since <- parse_datetime(user_data_small$yelping_since, format = "%Y-%m-%d %H:%M:%S", na = c("", "NA"), locale = default_locale(), trim_ws = TRUE)
 
 ##Create a new variable to count the number of friends a user has
-user_data_small$friends <- strsplit(user_data_small$friends, split = ",")
-user_data_small$friends_count <- sapply(user_data_small$friends, length)
+#user_data_small$friends <- strsplit(user_data_small$friends, split = ",")
+#user_data_small$friends_count <- sapply(user_data_small$friends, length)
+#friends variable may be useful if we could map the star ratings given by friends, but such procedure can be too computational heavy and hence I decided to omit it (for now). 
 
 #Drop user_data_small$friends
-user_data_small <- user_data_small %>%
-  select(-friends)
+#user_data_small <- user_data_small %>% #Dont drop first
+#  select(-friends)
 
-#Drop text and compliment counts in tip_data
+#Drop text in tip_data
 tip_data <- tip_data %>%
   mutate(tip = 1)
 tip_data <- tip_data %>%
-  select(user_id, business_id, tip)
-
-#create sentiment scores from review text
-sentiments <- textdata::lexicon_afinn()   # Get the sentiment lexicon
-tokens <- review_data_small %>%           # Tokenise the text
-  tidytext::unnest_tokens(word, text)
-sentiment_scores <- tokens %>%            # Calculate sentiment scores
-  inner_join(sentiments) %>%
-  group_by(review_id) %>%
-  summarise(sentiment_score = sum(score))
-#Add sentiment scores to review_data_small
-review_data_small <- left_join(review_data_small, sentiment_scores, by = "review_id")
+  select(-text)
 
 
 ## merge business data and checkin data
@@ -90,7 +109,7 @@ merge_business <- business_data %>%
 ## merge review data with all other data
 master_data <- review_data_small %>%
   left_join(user_data_small, by = "user_id") %>%
-  left_join(tip_data, by = c("user_id", "business_id")) %>%
+  left_join(tip_data, by = c("user_id", "business_id", "date")) %>%
   left_join(merge_business, by = "business_id")
 
 ## replace tip == 0 if NA
@@ -122,20 +141,26 @@ master_data <- master_data %>%
 
 #summary 
 glimpse(master_data)
+review_train <- na.omit(review_train)
 
+#if u have more time: turn RestaurantPriceRange2 into <dbl>, WheelchairAccessible into <lgl>, Alcohol & NoiseLevel into <list>, RestaurantsAttire into <lgl> if casual, expand dummy for "BusinessParking" "Ambience" "GoodForMeal"
 
 
 
 #Modelling
 #First split data into training and test
 set.seed(1)  #for reproducibility
-rows <- sample(nrow(merged_data))  #Shuffle row indices: rows
-shuffled_review <- merged_data[rows, ]  #Randomly order data
+rows <- sample(nrow(master_data))  #Shuffle row indices: rows
+shuffled_review <- master_data[rows, ]  #Randomly order data
 review_test <- shuffled_review[1:10000, ]  #test dataset
 review_train <- shuffled_review[10001:nrow(shuffled_review), ]  #training dataset
 
-#Linear Regression Model
-lm_model <- lm(stars.x ~ ., review_train)  #Fit linear model on training dataset
+#reduce size of training data
+review_train <- review_train %>%
+  select(stars.x, review_id, user_id, business_id, sentiment_score, review_count.x, useful.y, average_stars, tip, city, postal_code, stars.y, review_count.y, checkin_freq, is_elite)
+
+#Since stars rating is a discrete variable, a regression model will suffice to predict 
+lm_model <- lm(stars.x ~ sentiment_score + review_count.x + useful.y + average_stars + tip + city +  postal_code + stars.y + review_count.y + checkin_freq + is_elite, review_train)  #Fit linear model on training dataset
 predict <- predict(lm_model, review_test)  #Predict on test dataset
 error <- predict - review_test$stars.x     #Compute errors
 rmse <- sqrt(mean(error^2))                #Calculate RMSE
